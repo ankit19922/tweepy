84d725d5b131aa11169172648ed3cfa8cbcc1151
ankit19922 <31828052+ankit19922@users.noreply.github.com>
2019-11-15 03:56:22 +0000

    Add files via upload

---
+++ Tweepy.py
@@ -0,0 +1,29 @@
+import tweepy
+from textblob import TextBlob
+import pandas as pd
+import os
+tweetlist=[]
+tweet_type=[]
+
+Access_token = "1088749854774517760-cRbYAgEMeUeuwhVR6s8oHaXrKBRh32"
+Access_token_secret = "llBKfHQqgKEM1WqAadrLjYuuNFJIS3sgDElwiHTRXLRs3"
+Consumer_key = "EazL5W6t9GOMpZj243UEEKyvI"
+Consumer_secret_key = "SSP33bWsIyQTRA17Lnb36oBL8YoWbZbJfYjGWmtQxTpP4XqtxO"
+
+auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret_key)
+
+
+auth.set_access_token(Access_token, Access_token_secret)
+
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+tweets=tweepy.Cursor(api.search,q="Brexit",lang='en', since='2018-05-01').items(100000)
+
+for tweet in tweets:
+    tweetlist.append(tweet.text)
+    print(tweet.text)
+df=pd.DataFrame(tweetlist)
+#print(df)
+df.to_csv('D:/data.csv')
+
+
